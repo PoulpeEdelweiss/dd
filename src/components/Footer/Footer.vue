c73c93c2e0d647b720211674a4026df77e32d07e
PoulpeEdelweiss <natalina7231@gmail.com>
2024-04-28 02:13:27 +0300

    футер (сырой)

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="grid pl-[9px] py-[30px] bg-[#004978] h-[268px] text-sm leading-[17px] text-[#F9F9F9]">
+        <ul>
+            <li>
+                <p>© 2023 ДОННТУ</p>
+            </li>
+            <li class="py-[20px]">
+                <p class="pb-[7px]">Электронная почта оргкомитета олимпиады:</p>
+                <a href="olymp@donntu.ru">olymp@donntu.ru</a>
+            </li> 
+            <li>
+                <p class="pb-[7px]">Телефоны для справок:</p>
+                <a class="pb-[7px]" href="(+7949) 009-03-04">(+7949) 009-03-04</a>
+                <a href="(+7949) 320-37-65">(+7949) 320-37-65</a>
+            </li> 
+        </ul>
+        
+    </footer>
+</template>
+<script setup>
+</script>
