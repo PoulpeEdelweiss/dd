61f9be7ac7d60f918fc1f6fb1caae76862b4776d
PoulpeEdelweiss <natalina7231@gmail.com>
2024-04-30 00:31:28 +0300

    первый экран в адаптивном

---
+++ src/components/Concept/Concept.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="relative bg-white pb-[38px] lg:pb-[244px]">
+        <div class="pt-[126px] px-[9px] lg:pl-[150px]">
+            <h1 class="pb-[23px]  text-SemiBold-h1-mob lg:w-[804px] lg:text-SemiBold-h1-desc lg:pb-[12px]">Олимпиада по программированию</h1>
+            <p class="Light-h4-mob lg:text-Light-h4-desc pb-[31px] lg:pb-[37px] lg:w-[694px]">Кафедра «Программная инженерия им. Л.П. Фельдмана» Донецкого национального технического университетаприглашает принять участие»!</p>
+            <button class="text-SemiBold-button px-[27px] py-[15px] rounded-[10px] border-solid bg-[#00C073] text-[#FFFFFF] text-base">Зарегестрироваться</button>
+            <img class="absolute w-[118px] h-[91px] top-[35px] right-[25px] lg:top-[249px] lg:right-[112px] lg:w-[650px] lg:h-[494px]" src="icons/laptop.svg" alt="hi">
+        </div>
+       <div class="">
+            
+       </div>
+    </div>
+</template>
+<script>
+</script>
