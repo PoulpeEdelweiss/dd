26b65b89d9fd7c533f486e9f7f2aa6e04f444b84
PoulpeEdelweiss <natalina7231@gmail.com>
2024-04-30 04:02:22 +0300

    центрирование

--- src/components/CardReason/CardReason.vue
+++ src/components/CardReason/CardReason.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="bg-[#004978] text-[#F9F9F9] px-[8px] pt-[28px] pb-[32px] lg:px-[150px] lg:py-[64px]">
+    <div class="bg-[#004978] text-[#F9F9F9] pt-[28px] pb-[32px] w-full mx-auto px-[8px] lg:px-[150px] lg:py-[64px]">
         <h2 class="text-center pb-[25px] text-Regular-h2-mob lg:text-Regular-h2-desc lg:pb-[34px]">ПОЧЕМУ СТОИТ ПРИНЯТЬ УЧАСТИЕ В ОЛИМПИАДЕ?</h2>
-        <ul class="flex flex-col gap-[20px] lg:flex-row lg:gap-[14px] ">
+        <ul class="flex flex-col gap-[20px] lg:flex-row lg:gap-[14px] justify-center">
             <li class="max-w-[367px]">
                 <p class="text-SemiBold-h1-mob font-['Monoton'] pb-[10px] lg:pb-[14px]">1</p>
                 <p class="text-Light-h4-mob">Повысишь уровень интеллектуальных и творческих  возможстей по программированию.</p>
