0e77a504bfe45e2dd5855413c29bc244f623bc1a
PoulpeEdelweiss <natalina7231@gmail.com>
2024-05-03 05:00:31 +0300

    добавление компонента иконки

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -4,8 +4,8 @@
     <header class="transition-all duration-300">
     <div class="w-full mx-auto bg-[#F9F9F9] py-[17px] lg:py-[25px] lg:px-[150px]">
       <ul class="flex items-center justify-between px-[9px] mx-auto lg:grid grid-cols-8 lg:px-[0]">
-        <li class=" w-[76px] h-[50px] lg:col-start-1 lg:w-[100px] lg:h-[60px]">
-          <img src="icons/logo.svg" alt="logo">
+        <li class=" lg:col-start-1">
+          <Icon name="logo" size="lg ? 'w-[100px] h-[60px]' : 'w-[76px] h-[50px]'" />
         </li>
         <li class="col-end-6 py-[20px] lg:col-start-2">
           <nav class="flex justify-between text-Regular-menu text-[FFFFFF] transition-all duration-300 lg:block">
@@ -25,8 +25,8 @@
         </li>
         <li class="col-start-7 hidden max-w-max lg:block">
           <div class="flex items-center justify-center w-[50px] rounded-full bg-[#D1D1D1] h-[50px]">
-            <img class="" width="31" height="31" src="icons/search.svg" alt="">
-            
+            <!-- <img class="" width="31" height="31" src="icons/search.svg" alt=""> -->
+            <Icon name="search" size="lg ? 'h-[31px]' : ' h-[21px]'" />
           </div>
         </li>
         <li class="hidden lg:block max-w-max col-start-8">
@@ -42,5 +42,6 @@
 </template>
 
 <script setup>
+  import Icon from "@components/Icon/Icon.vue";
 </script>
 
---
+++ src/components/Icon/Icon.vue
@@ -0,0 +1,15 @@
+<template>
+	<svg :class="`${ size }`">
+		<use :xlink:href="`/icons/${file}.svg#${name}`"></use>
+	</svg>
+</template>
+
+<script setup>
+
+const props = defineProps({
+	file: {type: String, default: () => 'all-icons'},
+	name: { type: String, default: () => '' },
+	size: { type: String, default: () => '' }
+})
+</script>
+
