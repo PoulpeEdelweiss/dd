960eff01170c82b5d559f30cee6ef6e48eb8e610
PoulpeEdelweiss <natalina7231@gmail.com>
2024-05-04 07:03:09 +0300

    компонент контакты(недоделанный)

---
+++ src/components/Contacts/Contacts.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="mx-auto">
+        <h1 class="text-Regular-h2-mob lg:Regular-h2-desc">Контакты</h1>
+        <ul>
+            <li>
+
+            </li>
+        </ul>
+    </div>
+</template>
+<script setup>
+    
+</script>
